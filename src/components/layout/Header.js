a1f320fb2d8a2f1b849e6b0a5e0b1379e84ba0da
Sam Costanzo <samcostanzo25@gmail.com>
2019-06-01 21:16:57 -0400

    Add mobile layout

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
         <header  style = { headerStyle }>
-            <h1>Todo List</h1>
+            <h1>To Do List</h1>
             <Link style = { linkStyle } to = '/'>Home</Link> | <Link style = { linkStyle } to = '/about'>About</Link>
         </header>
     )
